1e120baa61cfc8f3dd53d3a75318c3f1f0d944cf
Jelte Fennema <github-tech@jeltef.nl>
2023-03-01 14:56:00 +0100

    Move test schema to a separate file

--- src/lib.rs
+++ src/lib.rs
@@ -6,75 +6,7 @@ use std::fmt;
 
 pgx::pg_module_magic!();
 
-extension_sql!(
-    r#"
-CREATE TABLE companies (
-  id bigserial PRIMARY KEY,
-  name text NOT NULL,
-  image_url text,
-  created_at timestamp without time zone NOT NULL,
-  updated_at timestamp without time zone NOT NULL
-);
-
-CREATE TABLE campaigns (
-  id bigserial,
-  company_id bigint REFERENCES companies (id),
-  name text NOT NULL,
-  cost_model text NOT NULL,
-  state text NOT NULL,
-  monthly_budget bigint,
-  blacklisted_site_urls text[],
-  created_at timestamp without time zone NOT NULL,
-  updated_at timestamp without time zone NOT NULL,
-  PRIMARY KEY (company_id, id)
-);
-
-CREATE TABLE ads (
-  id bigserial,
-  company_id bigint,
-  campaign_id bigint,
-  name text NOT NULL,
-  image_url text,
-  target_url text,
-  impressions_count bigint DEFAULT 0,
-  clicks_count bigint DEFAULT 0,
-  created_at timestamp without time zone NOT NULL,
-  updated_at timestamp without time zone NOT NULL,
-  PRIMARY KEY (company_id, id),
-  FOREIGN KEY (company_id, campaign_id)
-    REFERENCES campaigns (company_id, id)
-);
-
-CREATE TABLE clicks (
-  id bigserial,
-  company_id bigint,
-  ad_id bigint,
-  clicked_at timestamp without time zone NOT NULL,
-  site_url text NOT NULL,
-  cost_per_click_usd numeric(20,10),
-  user_ip inet NOT NULL,
-  user_data jsonb NOT NULL,
-  PRIMARY KEY (company_id, id),
-  FOREIGN KEY (company_id, ad_id)
-    REFERENCES ads (company_id, id)
-);
-
-CREATE TABLE impressions (
-  id bigserial,         -- was: PRIMARY KEY
-  company_id bigint,    -- added
-  ad_id bigint,         -- was: REFERENCES ads (id),
-  seen_at timestamp without time zone NOT NULL,
-  site_url text NOT NULL,
-  cost_per_impression_usd numeric(20,10),
-  user_ip inet NOT NULL,
-  user_data jsonb NOT NULL,
-  PRIMARY KEY (company_id, id),
-  FOREIGN KEY (company_id, ad_id)
-    REFERENCES ads (company_id, id)
-);
-"#,
-    name = "example_schema",
-);
+extension_sql_file!("schema.sql");
 
 #[derive(Debug)]
 struct DatabaseDescription {
---
+++ src/schema.sql
@@ -0,0 +1,64 @@
+CREATE TABLE companies (
+  id bigserial PRIMARY KEY,
+  name text NOT NULL,
+  image_url text,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL
+);
+
+CREATE TABLE campaigns (
+  id bigserial,
+  company_id bigint REFERENCES companies (id),
+  name text NOT NULL,
+  cost_model text NOT NULL,
+  state text NOT NULL,
+  monthly_budget bigint,
+  blacklisted_site_urls text[],
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  PRIMARY KEY (company_id, id)
+);
+
+CREATE TABLE ads (
+  id bigserial,
+  company_id bigint,
+  campaign_id bigint,
+  name text NOT NULL,
+  image_url text,
+  target_url text,
+  impressions_count bigint DEFAULT 0,
+  clicks_count bigint DEFAULT 0,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL,
+  PRIMARY KEY (company_id, id),
+  FOREIGN KEY (company_id, campaign_id)
+    REFERENCES campaigns (company_id, id)
+);
+
+CREATE TABLE clicks (
+  id bigserial,
+  company_id bigint,
+  ad_id bigint,
+  clicked_at timestamp without time zone NOT NULL,
+  site_url text NOT NULL,
+  cost_per_click_usd numeric(20,10),
+  user_ip inet NOT NULL,
+  user_data jsonb NOT NULL,
+  PRIMARY KEY (company_id, id),
+  FOREIGN KEY (company_id, ad_id)
+    REFERENCES ads (company_id, id)
+);
+
+CREATE TABLE impressions (
+  id bigserial,
+  company_id bigint,
+  ad_id bigint,
+  seen_at timestamp without time zone NOT NULL,
+  site_url text NOT NULL,
+  cost_per_impression_usd numeric(20,10),
+  user_ip inet NOT NULL,
+  user_data jsonb NOT NULL,
+  PRIMARY KEY (company_id, id),
+  FOREIGN KEY (company_id, ad_id)
+    REFERENCES ads (company_id, id)
+);
